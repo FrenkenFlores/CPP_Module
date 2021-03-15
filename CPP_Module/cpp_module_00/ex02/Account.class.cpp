a47b8a9c4230daaa8cbaf4d89381d13d10deefc8
FrenkenFlores <saifualdin.evloev@gmail.com>
2021-03-15 15:14:04 +0300

    update #6: recovered static getters

--- CPP_Module/cpp_module_00/ex02/Account.class.cpp
+++ CPP_Module/cpp_module_00/ex02/Account.class.cpp
@@ -6,14 +6,21 @@
 /*   By: fflores <fflores@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 15:47:03 by fflores           #+#    #+#             */
-/*   Updated: 2021/03/13 18:58:04 by fflores          ###   ########.fr       */
+/*   Updated: 2021/03/15 15:12:24 by fflores          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tests.hpp"
 
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
 Account::Account( int initial_deposit ){
 	this->_amount = initial_deposit;
+	Account::_totalAmount += initial_deposit;
+	this->_accountIndex = Account::getNbAccounts();
 	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ';' << "created" << std::endl;
 	return ;
 }
@@ -24,7 +31,7 @@ Account::~Account(){
 }
 
 int	Account::getNbAccounts( void ){
-	return _nbAccounts;
+	return _nbAccounts++;
 }
 
 int	Account::getTotalAmount( void ){
@@ -40,6 +47,6 @@ int	Account::getNbWithdrawals( void ){
 }
 
 void Account::displayAccountsInfos( void ){
-	std::cout << "accounts:" << Account::getNbAccounts() << "total:" << Account::getTotalAmount() << "deposits:" << Account::getNbDeposits() <<  "withdrawals:" << Account::getNbWithdrawals() << std::endl;
+	std::cout << "accounts:" << Account::getNbAccounts() << ";total:" << Account::getTotalAmount() << ";deposits:" << Account::getNbDeposits() <<  ";withdrawals:" << Account::getNbWithdrawals() << std::endl;
 	return ;
 }
