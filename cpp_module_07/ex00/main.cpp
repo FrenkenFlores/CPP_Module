7399ee76634c5f59b3b7f100e23c1f0b6c27a962
FrenkenFlores <fflores@MacBook-Pro-FrenkenFlores.local>
2021-03-31 23:55:35 +0300

    update #42: working on cpp_module_07, completed ex00

---
+++ cpp_module_07/ex00/main.cpp
@@ -0,0 +1,48 @@
+#include "whatever.hpp"
+#include "Awesome.hpp"
+
+int main( void ) {
+
+	PRINT("Subject tests");
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	PRINT("Complex type test");
+	Awesome obj1(1);
+	Awesome obj2(2);
+	std::cout << "before: obj1 = " << obj1.getN() << ", obj2 = " << obj2.getN() << std::endl;
+	::swap( obj1, obj2 );
+	std::cout << "after: obj1 = " << obj1.getN() << ", obj2 = " << obj2.getN() << std::endl;
+	std::cout << "min( obj1, obj2 ) = " << ::min( obj1, obj2 ).getN() << std::endl;
+	std::cout << "max( obj1, obj2 ) = " << ::max( obj1, obj2 ).getN() << std::endl;
+
+
+	PRINT("Optional tests : float");
+	float f1 = 0.345;
+	float f2 = 42.42;
+	std::cout << "before: f1 = " << f1 << ", f2 = " << f2 << std::endl;
+	::swap( f1, f2 );
+	std::cout << "after: f1 = " << f1 << ", f2 = " << f2 << std::endl;
+	std::cout << "min( f1, f2 ) = " << ::min( f1, f2 ) << std::endl;
+	std::cout << "max( f1, f2 ) = " << ::max( f1, f2 ) << std::endl;
+
+	PRINT("Optional tests : double");
+	double d1 = 0.345;
+	double d2 = 42.42;
+	std::cout << "before: d1 = " << d1 << ", d2 = " << d2 << std::endl;
+	::swap( d1, d2 );
+	std::cout << "after: d1 = " << d1 << ", d2 = " << d2 << std::endl;
+	std::cout << "min( d1, d2 ) = " << ::min( d1, d2 ) << std::endl;
+	std::cout << "max( d1, d2 ) = " << ::max( d1, d2 ) << std::endl;
+	return 0;
+}
---
+++ cpp_module_07/ex00/whatever.hpp
@@ -0,0 +1,27 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <string>
+#include <iostream>
+#define PRINT(str) std::cout << "\033[1;32m" << str << "\033[0m" << std::endl
+
+
+template <typename T>
+void swap(T &obj1, T &obj2) {
+	T tmp = obj1;
+	obj1 = obj2;
+	obj2 = tmp;
+	return;
+}
+
+template <typename T>
+const T & min(const T & obj1, const T &obj2) {
+	return (obj1 < obj2 ? obj1 : obj2);
+}
+
+template <typename T>
+const T & max(const T & obj1, const T &obj2) {
+	return (obj1 > obj2 ? obj1 : obj2);
+}
+
+#endif
